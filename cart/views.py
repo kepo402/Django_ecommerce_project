9695e6b4766576dd6564e09470c953dc9234f094
kepo402 <alawodeolayinka2@gmail.com>
2023-12-19 17:10:51 +0100

    commit changes

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from product.models import Product
+from  .models import Cart, Order
 
 # Create your views here.
+def addtocart(request, slug):
+    item = get_object_or_404(Product, slug=slug)
+    order_item, created = Cart.objects.get_or_create(
+        item= item,
+        user = request.user
+        ) 
+    
